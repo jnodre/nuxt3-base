b94492c62398f428ccdd26dd81ec14ed161ca908
Josué Noda <josunoda@gmail.com>
2022-12-29 13:55:56 +0000

    arreglo error

--- error.vue
+++ error.vue
@@ -1,4 +1,39 @@
 <template>
-  <h1>Página no encontrada</h1>
-  <nuxt-link to="/">Volver a la home</nuxt-link>
+  <Breadcrumbs title="Página no encontrada" />
+  <main class="error">
+    <div class="error__image">
+      <img src="https://dummyimage.com/1000x600.jpg" alt="" />
+    </div>
+    <h1 class="error__title">Página no encontrada</h1>
+    <h2 class="error__description">
+      ¡Lo sentimos! Parece que la página que buscas no existe, tal vez te
+      interese alguna de las siguientes páginas:
+    </h2>
+    <nav>
+      <ul class="error__menu">
+        <li>
+          <nuxt-link class="button button--full" to="/servicios"
+            >Servicios</nuxt-link
+          >
+        </li>
+        <li>
+          <nuxt-link class="button button--full" to="/sobre-asimetrica"
+            >Sobre Asimétrica</nuxt-link
+          >
+        </li>
+        <li>
+          <nuxt-link class="button button--full" to="/publicaciones"
+            >Publicaciones</nuxt-link
+          >
+        </li>
+        <li>
+          <nuxt-link class="button button--full" to="/contacto"
+            >Agenda una llamada / Contacto</nuxt-link
+          >
+        </li>
+      </ul>
+    </nav>
+  </main>
+  <WorkUs />
+  <Subscribe />
 </template>
